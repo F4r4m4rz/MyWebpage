ab7d098a3f2cda2e17b3e29596c99c601b53c6a2
Faramarz Bodaghi <faramarz.bodaghi@outlook.com>
2020-08-19 21:58:57 +0200

    Tried making a fixed picture successfully

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let element = document.getElementsByClassName("nav-button");
+element.addEventListener("load", function() {
+    element.scroll();
+})
+
+function ChangeClass() {
+    let element = document.getElementById("navigation");
+    element.addEventListener("load", function() {
+        alert("Loaded");
+    })
+    if (element.classList.contains("nav-collapse")) {
+        element.classList.remove("nav-collapse");
+        element.classList.add("nav-show");
+    } else {
+        element.classList.remove("nav-show");
+        element.classList.add("nav-collapse");
+    }
+};
